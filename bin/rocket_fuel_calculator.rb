71c4fc929d0306c0605c0884696bc2af78a263f4
Ilko Fedak <oklika@gmail.com>
2024-04-28 14:41:06 +0300

    add command line utility to calculate required fuel

---
+++ bin/rocket_fuel_calculator.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require_relative '../lib/rocket_launcher'
+
+mass = ARGV[1].to_i
+path = ARGV[2..-1]&.each_slice(2)&.map { |action, target| [action.to_sym, target&.downcase] }
+
+def print_usage
+  puts "Usage: rocket_fuel_calculator <mass> <path>"
+  puts "Example: rocket_fuel_calculator 28801 launch earth land moon launch moon land earth"
+end
+
+if mass <= 0 || path.empty? || path.any? { |action, target| ![:launch, :land].include?(action) }
+  print_usage
+  exit(1)
+end
+
+total_fuel = RocketLauncher.new(mass, path).fuel_required
+
+if total_fuel
+  puts "Total rocket fuel required: #{total_fuel} kg"
+end
